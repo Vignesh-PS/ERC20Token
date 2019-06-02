36286fabb893652a48daea9c5bcf6e0adc578e70
Vignesh-PS <selvavignesh128@gmail.com>
2019-06-02 14:24:32 +0530

    first commit

---
+++ contracts/token.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.5.8;
+
+contract ERC20Basic {
+
+    string public  name = "ERC20";
+    string public  symbol;
+    uint8 public  decimals = 18;  
+
+
+    event Approval(address  tokenOwner, address  spender, uint tokens);
+    event Transfer(address  from, address  to, uint tokens);
+
+
+    mapping(address => uint) balances;
+
+    mapping(address => mapping (address => uint256)) allowed;
+    
+    uint256 totalSupply_;
+
+
+
+   constructor(uint total) public {  
+	totalSupply_ = total;
+	balances[msg.sender] = totalSupply_;
+    }  
+
+    function totalSupply() public view returns (uint256) {
+	return totalSupply_;
+    }
+    
+    function balanceOf(address tokenOwner) public view returns (uint) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] -= numTokens;
+        balances[receiver] += numTokens;
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] += numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[owner]);    
+        require(numTokens <= allowed[owner][msg.sender]);
+        balances[owner] -= numTokens;
+        allowed[owner][msg.sender]-= numTokens;
+        balances[buyer] += numTokens;
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+}
